8ff768a0f8c44eb18d410fe7b87d65bd8c1255fc
boan85 <boan85@gmail.com>
2019-02-14 15:26:30 +0200

    lead migration

--- leads/migrations/0003_auto_20190211_2146.py
+++ leads/migrations/0003_auto_20190211_2146.py
@@ -7,7 +7,6 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('contacts', '0002_auto_20190211_2106'),
         ('leads', '0002_lead_tags'),
     ]
 
